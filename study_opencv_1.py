4036cc2a7511dad22fc2e58bf9ba2c9f42c7a602
liwuyou <3125172535@qq.com>
2023-11-01 23:31:17 +0800

    opencv的first_file

---
+++ study_opencv_1.py
@@ -0,0 +1,27 @@
+# 学习了基本操作，imread(), imwrite()等
+import cv2 as cv
+
+# 获取摄像头
+cap = cv.VideoCapture(2)
+# set the height(3) and width(4) of the windows
+# windows系统下，微软surface的系统摄像头，该设置失效，原因未明
+cap.set(3, 640)
+cap.set(4, 480)
+
+# set brightness
+# 但好像也没什么用
+cap.set(10, 0.00001)
+
+while True:
+    success, img = cap.read()
+
+    cv.imshow("camera", img)
+    if cv.waitKey(1) == ord('Q') or cv.waitKey(1) == 27:
+
+        # 当所有事完成，释放 VideoCapture 对象
+        cap.release()
+        cv.destroyAllWindows()
+        break
+
+
+
---
+++ test_file.py
@@ -0,0 +1,24 @@
+import cv2
+
+cap = cv2.VideoCapture(2)
+
+print('ok')
+
+cap.set(3, 300)
+cap.set(4, 300)  # 设置摄像头分辨率，3为高，4为宽
+
+while True:
+    ret, frame = cap.read()
+
+    # 设置显示的窗口大小为500,500，建议大于等于摄像头分辨率
+    # cv2.resizeWindow("Local Camera", 300, 300)
+
+    cv2.imshow("Local Camera", frame)
+
+    # 移动当前显示窗口至（0,0）
+    # cv2.moveWindow("Local Camera", 0, 0)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
