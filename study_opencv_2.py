c5b37bf2614cfb79f3dbfee58eced6631bb0290d
liwuyou <3125172535@qq.com>
2023-11-03 08:45:32 +0800

    2 commit_2

--- study_opencv_2.py
+++ study_opencv_2.py
@@ -1,4 +1,7 @@
 import cv2 as cv
+import numpy as np
+
+aaa = np.ones((5, 5), np.uint8)
 
 # cv.imread("路径"， 参数)，参数不传，默认参数1
 img = cv.imread("img/1 (1).jpg")
@@ -13,7 +16,20 @@ cv.imshow("Local Camera", img)
 imgGray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 cv.imshow("show imgGray", imgGray)
 
+# 高斯模糊
 imgBlur = cv.GaussianBlur(img, (7, 7), 0)
 cv.imshow("show imgBlur", imgBlur)
+
+# 边缘检测
+imgCanny = cv.Canny(img, 150, 200)
+cv.imshow("show imgCanny", imgCanny)
+
+# 膨胀
+imgDilation = cv.dilate(imgCanny, aaa, iterations=1)
+cv.imshow("show imgDilation", imgDilation)
+
+# 腐蚀
+imgErode = cv.erode(imgDilation, aaa, iterations=1)
+cv.imshow("show imgErode", imgErode)
 cv.waitKey(0)
 
